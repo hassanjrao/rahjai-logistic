015bbb9d26adede6bfa437098ce490c29c795e16
Hassan Javaid <hassanjrao@gmail.com>
2024-11-07 13:02:02 +0400

    added tawk to

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -383,6 +383,23 @@
     <script src="{{ asset('front-assets/js/custom.js') }}"></script>
 
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/672c809e2480f5b4f59a1aad/1ic2tcrkk';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
+
 </body>
 
 
